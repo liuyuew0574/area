ac01ba999d50f7799c927cbbadf66f531a2720ac
liuyw <liuyw7224.com>
2019-04-02 15:14:44 +0800

    加了新城市

--- dist/index.js
+++ dist/index.js
@@ -19,6 +19,10 @@ export default class area {
     this.country = "";
     this.povice = "";
     this.city = "";
+    this.continentId = 1;
+    this.countryId = 7;
+    this.poviceId = "";
+    this.cityId = "";
   }
   AreaLoc(isAsia)//需要洲为true
   {
@@ -52,9 +56,70 @@ export default class area {
   }
   //点击省份 求国家
   provinceChange(index, name) {
-    debugger
     const that = this;
     that.povice = name;
     that.cityRegion = that.provinceRegion[index].subAreaLoc
+  }
+
+
+
+
+
+
+
+
+
+  ///新版
+  //获取地区
+  newAreaLoc(isAsia) {
+    const that = this;
+    that.api.AreaLoc().then(function (res) {
+      if (res.isCompleted) {
+        that.subAreaList = res.data;//res.data[0]亚洲  res.data[0].subAreaLoc 中国
+        if (isAsia == false)//默认给亚洲
+        {
+          that.newContinentChange()
+          that.newCountryChange()
+        }
+      } else {
+        that.$message.error(res.message)
+      }
+    })
+  }
+  //点击洲 求国家
+  newContinentChange() {
+    const that = this;
+    that.subAreaList.map(function (item, index) {
+      if (item.areaId == that.continentId) {
+        that.continentRegion = item.subAreaLoc
+      }
+    })
+  }
+  //点击国家 求省份
+  newCountryChange(areaId) {
+    const that = this;
+    // that.countryId = areaId;
+
+    that.continentRegion.map(function (item, index) {
+      if (areaId == undefined) {
+        if (item.areaId == that.countryId) {
+          that.provinceRegion = item.subAreaLoc
+        }
+      }
+      else {
+        if (item.areaId == 1) {
+          that.provinceRegion = item.subAreaLoc
+        }
+      }
+    })
+  }
+  //点击省份 求城市
+  newProvinceChange() {
+    const that = this;
+    that.provinceRegion.map(function (item, index) {
+      if (item.areaId == that.poviceId) {
+        that.cityRegion = item.subAreaLoc
+      }
+    })
   }
 }
